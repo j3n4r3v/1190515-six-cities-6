c32f065286705df318d647acb1d11a660fe3bc8f
j3n4r3v <jene4ka4reva@gmail.com>
2021-02-17 23:14:55 +0200

    add comments.js

---
+++ src/mocks/comments.js
@@ -0,0 +1,39 @@
+export const reviews = [
+  {
+    comment: `I stayed here for one night and it was an unpleasant experience.`,
+    date: `2021-01-23T08:17:53.220Z`,
+    id: 1,
+    rating: 3,
+    user: {
+      avatarUrl: `img/avatar-max.jpg`,
+      id: 13,
+      isPro: false,
+      name: `Max`
+    }
+  },
+  {
+    comment: `I stayed here for one night and it was an unpleasant experience.`,
+    date: `2021-2-21T08:36:38.657`,
+    id: 2,
+    rating: 3,
+    user: {
+      avatarUrl: `img/avatar-angelina.jpg`,
+      id: 16,
+      isPro: true,
+      name: `Angelina`
+    }
+  },
+  {
+    comment: `Beautiful space, fantastic location and atmosphere, really a wonderful place to spend a few days. Will be back.`,
+    date: `2021-02-25T09:36:38.657Z`,
+    id: 3,
+    rating: 4,
+    user: {
+      id: 14,
+      isPro: true,
+      name: `Max`,
+      avatarUrl: `img/avatar-max.jpg`
+    }
+  }
+];
+
