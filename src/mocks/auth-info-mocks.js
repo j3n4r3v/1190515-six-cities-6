b96256d6cfa15495496a10c84d11faa4431818d6
j3n4r3v <jene4ka4reva@gmail.com>
2021-03-17 08:51:00 +0200

    hide mocks

--- src/mocks/auth-info-mocks.js
+++ src/mocks/auth-info-mocks.js
@@ -1,7 +1,7 @@
-export const authInfoMocks = {
-  "avatarUrl": `img/logo.svg`,
-  "email": `Oliver.conner@gmail.com`,
-  "id": 1,
-  "isPro": false,
-  "name": `Oliver.conner`
-};
+// export const authInfoMocks = {
+//   "avatarUrl": `img/logo.svg`,
+//   "email": `Oliver.conner@gmail.com`,
+//   "id": 1,
+//   "isPro": false,
+//   "name": `Oliver.conner`
+// };
