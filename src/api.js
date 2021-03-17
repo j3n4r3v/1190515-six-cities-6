04aa2eb7d1299a216f3b4bcd64ae393d73917ad5
j3n4r3v <jene4ka4reva@gmail.com>
2021-03-17 09:07:53 +0200

    questions with api - fetch and add/del comment

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL = `https://6.react.pages.academy/six-cities`;
 const REQUEST_TIMEOUT = 5000;
 
-const HttpCode = {
+const HttpCode = { // обработка ошибок как?
   UNAUTHORIZED: 401,
 };
 
@@ -20,7 +20,7 @@ export const createAPI = (onUnauthorized) => {
     const {response} = err;
 
     if (response.status === HttpCode.UNAUTHORIZED) {
-      onUnauthorized();
+      onUnauthorized(); // что конкретно делает этот callback?
 
       throw err;
     }
@@ -33,3 +33,93 @@ export const createAPI = (onUnauthorized) => {
   return api;
 
 };
+
+// import AdapterModel from "../models/adapter";
+
+// const Method = {
+//   GET: `GET`,
+//   PUT: `PUT`,
+//   POST: `POST`,
+//   DELETE: `DELETE`
+// };
+
+// const SuccessHTTPStatusRange = {
+//   MIN: 200,
+//   MAX: 299
+// };
+
+// export default class Api {
+//   constructor(endPoint, authorization) {
+//     this._endPoint = endPoint;
+//     this._authorization = authorization;
+//   }
+
+//   getFilms() {
+//     return this._sendRequest({
+//       url: `movies`
+//     })
+//       .then(Api.toJSON)
+//       .then(AdapterModel.createFilms);
+//   }
+
+//   updateFilm(id, film) {
+//     return this._sendRequest({
+//       url: `movies/${id}`,
+//       method: Method.PUT,
+//       body: JSON.stringify(film.adaptToServer()),
+//       headers: new Headers({ "Content-Type": `application/json` })
+//     })
+//       .then(Api.toJSON)
+//       .then(AdapterModel.createFilm);
+//   }
+
+//   getComment(id) {
+//     return this._sendRequest({
+//       url: `comments/${id}`
+//     })
+//       .then(Api.toJSON);
+//   }
+
+//   addComment(id, comment) {
+//     return this._sendRequest({
+//       url: `comments/${id}`,
+//       method: Method.POST,
+//       body: comment,
+//       headers: new Headers({ "Content-Type": `application/json` })
+//     });
+//   }
+
+//   deleteComment(id) {
+//     return this._sendRequest({
+//       url: `comments/${id}`,
+//       method: Method.DELETE
+//     });
+//   }
+
+//   _sendRequest({ url, method = Method.GET, body = null, headers = new Headers() }) {
+//     headers.append(`Authorization`, this._authorization);
+
+//     return fetch(`${this._endPoint}/${url}`, { method, body, headers })
+//       .then(Api.checkStatus)
+//       .catch(Api.catchError);
+//   }
+
+//   static checkStatus(response) {
+//     if (
+//       response.status < SuccessHTTPStatusRange.MIN &&
+//       response.status > SuccessHTTPStatusRange.MAX
+//     ) {
+//       throw new Error(`${response.status}: ${response.statusText}`);
+//     }
+
+//     return response;
+//   }
+
+//   static toJSON(response) {
+//     return response.json();
+//   }
+
+//   static catchError(err) {
+//     throw err;
+//   }
+// }
