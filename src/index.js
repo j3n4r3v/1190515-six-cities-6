84fce6e44f91cbe234b55a6eeadc3c1a6ed53d6b
j3n4r3v <jene4ka4reva@gmail.com>
2021-03-28 20:10:08 +0300

    change index

--- src/index.js
+++ src/index.js
@@ -4,29 +4,31 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import {createAPI} from "./api";
 
-import {composeWithDevTools} from "redux-devtools-extension";
-import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
-import thunk from "redux-thunk";
 
 import {setAuthInfo} from "./store/actions";
 import {reducer} from "./store/reducer";
 import {fetchOffersList, checkAuthStatus} from "./store/api-actions";
 import {redirect} from "./store/redirect";
 
-// import {AuthorizationStatus} from "./const";
+import {configureStore} from '@reduxjs/toolkit';
 
 const api = createAPI(
     () => store.dispatch(setAuthInfo(null))
 );
 // createAPI принимает callback который нужно вызвать на случай неавторизованности, обновит store
 
-const store = createStore(reducer,
-    composeWithDevTools(
-        applyMiddleware(thunk.withExtraArgument(api)),
-        applyMiddleware(redirect)
-    ));
-
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      thunk: {
+        extraArgument: api
+      }
+    })
+      .concat(redirect)
+});
 store.dispatch(checkAuthStatus());
 store.dispatch(fetchOffersList());
 
